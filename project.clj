0ac865116177e43f3475511192a2c720e119a32c
Ilmari Vacklin <ilmari.vacklin@cs.helsinki.fi>
2011-01-22 23:50:27 +0200

    Up Midje version to 1.0.1

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-                     [midje "1.0.0-COLLECTORS-EDITION"]])
+                     [midje "1.0.1"]])
